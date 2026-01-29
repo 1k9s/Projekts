dafad3ebd5660737578c15fe8c93b41416b950a1
1k9s <147737190+1k9s@users.noreply.github.com>
2026-01-29 19:07:20 +0200

    dokumentācija.txt + macibu_planotajs.py

--- macibu_planotajs.py
+++ macibu_planotajs.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 kopejais_laiks = 0
 darba_laiks = 0
 atlikusais_laiks = 0
+atputas_laiks = 0
 
 # saraksta attēlošana
 atputa = True
@@ -11,10 +12,9 @@ atputa = True
 saraksts = []
 
 pievieno = False
-atnem = False # implementēt
 
 # Dzēš ievades tekstu, kad uz tā uzspiež
-def clear_on_focus(event):
+def tira_ievadi(event):
     event.widget.delete(0, tk.END)
 
 # Ievadīto laiku limitē līdz 59
@@ -47,22 +47,16 @@ def limite_laiku():
     pievieno_laiku()
 
 def atpakal():
-    global darba_laiks, pievieno, atlikusais_laiks, kopejais_laiks
+    global darba_laiks, pievieno, kopejais_laiks
     kopejais_laiks = 0
     darba_laiks = 0
-    atlikusais_laiks = 0
     pievieno = False
-    atnem = False
-
-    for widget in box_frame.winfo_children():
-        widget.destroy()
 
     galvenais_skats.place_forget()
-    box_frame.place_forget()
     sakuma_skats.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 def izveido_sarakstu():
-    global darba_laiks, kopejais_laiks, saraksts
+    global darba_laiks, atputas_laiks, kopejais_laiks, saraksts
 
     darba_laiks -= 1 # pievieno_laiku() dod 1 par daudz
 
@@ -72,7 +66,7 @@ def izveido_sarakstu():
             return
 
     atlikusais = kopejais_laiks
-    atputas_laiks = darba_laiks /5
+    atputas_laiks = darba_laiks // 5
 
     # kamēr atlikušais ir 
     while atlikusais > 0:
@@ -86,7 +80,8 @@ def izveido_sarakstu():
             #saraksts.append(darba_laiks + int(atlikusais))
             atlikusais = 0
 
-    parstat_poga.place_forget() # Noņem pogu
+    atpakal_poga.pack_forget() # Noņem atpakaļ pogu
+    parstat_poga.place_forget() # Noņem pārstāt pogu
     attelo_sarakstu()
 
 # Skaita uz priekšu darba laiku
@@ -105,9 +100,6 @@ def pievieno_laiku():
 # Skaita uz atpakaļ atlikušo intervāla laiku
 def atnem_laiku():
     global atlikusais_laiks, saraksts, atputa
-
-    if len(saraksts) <= 0:# kad paliek 0 tad error
-        return
 
     if atlikusais_laiks >= 0:
         hours, remainder = divmod(atlikusais_laiks, 3600)
@@ -137,19 +129,29 @@ def attelo_sarakstu():
 
         if atputa:
             color = "#4B0F1C"
+            text = formate_laiku(atputas_laiks)
         else:
             color = "#0F2D4B"
+            text = formate_laiku(darba_laiks)
 
         if first: # Pirmā
             box = tk.Frame(box_frame, bg=color, width=110, height=30, highlightbackground="white", highlightthickness=1)
             box.place(relx=0.2,rely=0.18 + y, anchor="w")
 
-            uzraksts.configure(text="Atpūta")
+            uzraksts.configure(text="Atpūta") # Maina nosaukumu
+            box_frame.configure(bg="#160404") # Fona krāsu
+            galvenais_skats.configure(bg="#160404")
+            laiks.configure(bg="#160404")
+            uzraksts.configure(bg="#160404")
+            virs_teksts.configure(bg="#160404")
 
             first = False
         else: # Pārējās
             box = tk.Frame(box_frame, bg=color, width=110, height=30)
             box.place(relx=0.2,rely=0.18 + y, anchor="w")
+            box.pack_propagate(False) # Lai teksts nemaina izmēru
+            box_teksts = tk.Label(box, text=text, bg = color, fg="white")
+            box_teksts.pack(expand=True)
         
         #if len(saraksts) % 1 == 0: # Ja sarakstā intervālu skaits nav pāra skaitlis
         atputa = not atputa
@@ -160,7 +162,7 @@ def attelo_sarakstu():
     atnem_laiku()
 
 def partaisa_sarakstu():
-    global pievieno, atlikusais_laiks, saraksts, atputa
+    global pievieno, atlikusais_laiks, atputas_laiks, darba_laiks, saraksts, atputa
     first = True
     y = 0
 
@@ -175,8 +177,10 @@ def partaisa_sarakstu():
 
         if a:
             color = "#4B0F1C"
+            text = formate_laiku(atputas_laiks)
         else:
             color = "#0F2D4B"
+            text = formate_laiku(darba_laiks)
 
         if first: # Pirmā
             box = tk.Frame(box_frame, bg=color, width=110, height=30, highlightbackground="white", highlightthickness=1)
@@ -185,19 +189,47 @@ def partaisa_sarakstu():
             # Maina uzrakstu balstoties uz pirmā
             if atputa:
                 uzraksts.configure(text="Atpūta")
+                box_frame.configure(bg="#160404")
+                galvenais_skats.configure(bg="#160404")
+                laiks.configure(bg="#160404")
+                uzraksts.configure(bg="#160404")
+                virs_teksts.configure(bg="#160404")
             else:
                 uzraksts.configure(text="Darbs")
+                box_frame.configure(bg="#040D16")
+                galvenais_skats.configure(bg="#040D16")
+                laiks.configure(bg="#040D16")
+                uzraksts.configure(bg="#040D16")
+                virs_teksts.configure(bg="#040D16")
             first = False
         else: # Visas pārējās
             box = tk.Frame(box_frame, bg=color, width=110, height=30)
             box.place(relx=0.2,rely=0.18 + y, anchor="w")
+            box.pack_propagate(False) # Lai teksts nemaina izmēru
+            box_teksts = tk.Label(box, text=text, bg = color, fg="white")
+            box_teksts.pack(expand=True)
         
         a = not a
         y += 0.1
 
-    # aizsūta šī brīža intervāla laiku uz skaitītāju
-    atlikusais_laiks = int(saraksts[0])
-    atnem_laiku()
+    # Aizsūta šī brīža intervāla laiku uz skaitītāju
+    if len(saraksts) <= 0:
+        box_frame.configure(bg="#000000")
+        galvenais_skats.configure(bg="#000000")
+        laiks.configure(bg="#000000", text=" ")
+        uzraksts.configure(bg="#000000", text="Darbs pabeigts!")
+        virs_teksts.configure(bg="#000000", text=" ")
+        return
+    else:
+        atlikusais_laiks = int(saraksts[0])
+        atnem_laiku()
+    
+# Sekundes formatē minūtēs un stundās
+def formate_laiku(seconds):
+    seconds = int(seconds) # Drošībai
+    hours, remainder = divmod(seconds, 3600)
+    mins, secs = divmod(remainder, 60)
+    return f"{hours:02d}:{mins:02d}:{secs:02d}"
         
 #________________________________________________________________________________________________________________________________________
 
@@ -223,15 +255,15 @@ timeFrame.place(relx=0.5, rely=0.45, anchor="center")
 # Stundas
 h_ievade = tk.Entry(timeFrame, bg="#1c1c1c", fg = "white", font=("Arial", 26), width=3, justify="center", bd=0)
 h_ievade.insert(0, "00")
-h_ievade.bind("<FocusIn>", clear_on_focus)
+h_ievade.bind("<FocusIn>", tira_ievadi)
 # Minūtes
 min_ievade = tk.Entry(timeFrame, bg="#1c1c1c", fg = "white", font=("Arial", 26), width=3, justify="center", bd=0)
 min_ievade.insert(0, "00")
-min_ievade.bind("<FocusIn>", clear_on_focus)
+min_ievade.bind("<FocusIn>", tira_ievadi)
 # Sekundes
 sec_ievade = tk.Entry(timeFrame, bg="#1c1c1c", fg = "white", font=("Arial", 26), width=3, justify="center", bd=0)
 sec_ievade.insert(0, "00")
-sec_ievade.bind("<FocusIn>", clear_on_focus)
+sec_ievade.bind("<FocusIn>", tira_ievadi)
 
 h_ievade.pack(side="left")
 tk.Label(timeFrame, text=" : ", bg="#1c1c1c", fg = "white", font=("Arial", 24)).pack(side="left")
@@ -251,7 +283,7 @@ galvenais_skats = tk.Frame(logs, bg="#040D16")
 uzraksts = tk.Label(galvenais_skats, text="Darbs", font=("Arial", 30),fg="white",bg="#040D16")
 uzraksts.place(relx=0.6, rely=0.1, anchor="center")
 
-# Laika teksts
+# Teksts virs laika
 virs_teksts = tk.Label(galvenais_skats, text="Intervala laiks:", font=("Arial", 12),fg="#adadad",bg="#040D16")
 virs_teksts.place(relx=0.6, rely=0.4, anchor="center")
 
@@ -262,10 +294,11 @@ laiks.place(relx=0.6, rely=0.5, anchor="center")
 # Pārstāt darbu
 parstat_poga = tk.Button(galvenais_skats, text="Pārstāt darbu", width=10, font=("Arial", 10), bg="#040D16", fg = "white", command=izveido_sarakstu)
 
+# Intervālu rāmis
 box_frame = tk.Frame(galvenais_skats, bg="#040D16")
 
 # Atpakaļ
-atpakal = tk.Button(galvenais_skats, text="<", width=3, font=("Arial", 14), bg="#040D16", fg = "white", command=atpakal)
-atpakal.pack(anchor="w")
+atpakal_poga = tk.Button(galvenais_skats, text="<", width=3, font=("Arial", 14), bg="#000000", fg = "white", command=atpakal)
+atpakal_poga.pack(anchor="w")
 
 logs.mainloop()
